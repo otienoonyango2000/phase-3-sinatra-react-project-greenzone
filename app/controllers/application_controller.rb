3606060625f35bd0755a4b5f3ab90cd324151d0c
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-03-02 03:30:48 +0300

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,49 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/movies' do
+    movies = Movie.all 
+    movies.to_json
   end
+
+
+  #add movies
+  post "/movies" do
+    title = params[:title]
+    year = params[:year]
+    runtime = params[:runtime]
+    director = params[:director]
+    actors = params[:actors]
+    plot = params[plot]
+    posterUrl = params[posterUrl]
+
+    post = Movie.create(title: title, year: year, runtime: runtime, director: director, actors: actors,
+    plot: plot, posterUrl: posterUrl)
+  end
+
+  #delete a movie
+  delete "/movies/:id" do
+    count_movies = Movie.where(id: params[:id]).count() 
+          movie = Movie.find(params[:id])
+          movie.destroy
+
+  end
+
+ #edit
+  # patch '/movies/:id' do
+  #   check_movie_existence = Post.exists?(id: params[:id])
+  #   title = params[:title]
+  #   year = params[:year]
+  #   runtime = params[:runtime]
+  #   director = params[:director]
+  #   actors = params[:actors]
+  #   plot = params[plot]
+  #   posterUrl = params[posterUrl]
+
+  #   movie = Movie.find_by(id: params[:id])
+  #   Movie.update(title: title, year: year, runtime: runtime, director: director, actors: actors,
+  #   plot: plot, posterUrl: posterUrl)
+  #   # post.update(title: title, content: content, user_id: user)
+  # end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Movie.create(title:'Matrix', runtime:'945', director:'felix', actors:'ben', plot:'the good the wwwworde')
+Movie.create(title:'The Batman', runtime:'145', director:'sandra', actors:'bruce', plot:'the dark knight')
 
 puts "✅ Done seeding!"
